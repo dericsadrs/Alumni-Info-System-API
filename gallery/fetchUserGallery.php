dcadafd0734fdafab4ad81e0d690753d0705ffd9
dericsadrs <drccsadrs@gmail.com>
2022-11-29 16:00:30 +0800

    Added Gallery Delete

--- gallery/fetchUserGallery.php
+++ gallery/fetchUserGallery.php
@@ -1,7 +1,7 @@
 <?php   
             // mysqli_connect("localhost", username, password, database)
             $connect = mysqli_connect("localhost", "u693530993_alumni_db1","Alumniinformationsystem2022","u693530993_alumni_db1");
-           
+            $user_id = $_POST['user_id'];
            if(!$connect) 
            {
                 echo "Error Connecting to the Database!";
@@ -10,7 +10,7 @@
           else if( $connect) 
           {
 
-         $sql =  "SELECT galleries.id, galleries.image, galleries.description, galleries.created_at FROM galleries INNER JOIN users ON users.id = galleries.user_id  WHERE galleries.status = 1";
+         $sql =  "SELECT galleries.id, galleries.image, galleries.description, galleries.created_at FROM galleries  WHERE galleries.user_id = '$user_id'";
         $result = mysqli_query($connect, $sql);  
         $json_array = array();  
            while($row = mysqli_fetch_assoc($result))  
